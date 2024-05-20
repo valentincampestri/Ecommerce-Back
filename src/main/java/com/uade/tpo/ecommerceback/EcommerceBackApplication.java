507b69fe59aecc298ea26ee8c698b78ad7f2088f
Costa-Julian <costa-julian@outlook.com>
2024-05-20 20:04:06 -0300

    app inicial

---
+++ src/main/java/com/uade/tpo/ecommerceback/EcommerceBackApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.ecommerceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackApplication.class, args);
+	}
+
+}
