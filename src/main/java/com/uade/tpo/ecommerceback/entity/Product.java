92968da18906eaac3dc81753cc72bc9ab87075ab
francisco191 <silfrancisco99@gmail.com>
2024-05-26 19:40:28 -0300

    Creo entidad Product

--- src/main/java/com/uade/tpo/ecommerceback/entity/Product.java
+++ src/main/java/com/uade/tpo/ecommerceback/entity/Product.java
@@ -1,5 +1,26 @@
 package com.uade.tpo.ecommerceback.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Double precio;
 }
